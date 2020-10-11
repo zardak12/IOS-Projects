e11a7c2b85610b40937eea2275df016eed18cd13
mark <shmarkyr@gmail.com>
2020-10-11 14:40:33 +0300

    new commit

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Марк Шнейдерман on 18.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
